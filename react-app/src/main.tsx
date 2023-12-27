c589f7b5f6df464f7f95bb966c754e75c0eaf4ca
HyunwookJung0827 <as9804@berkeley.edu>
2023-12-26 21:20:31 -0800

    Finished "How React Works." next-> "React Ecosystem"

--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.css'
 
+// "npm run dev" to run
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <React.StrictMode> {/* Built-in component in react that doesn't have a visual representation. Its purpose is to identify potential problems */}
     <App />
   </React.StrictMode>,
 )
