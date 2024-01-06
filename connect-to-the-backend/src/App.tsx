edc0f665d69ea120bb092983e4a9e0718bb2ce10
HyunwookJung0827 <as9804@berkeley.edu>
2024-01-06 12:04:26 -0800

    [8-Working with Async and Await]

--- connect-to-the-backend/src/App.tsx
+++ connect-to-the-backend/src/App.tsx
@@ -65,9 +65,44 @@ import ProductList from "./components/ProductList";
 //   </div>
 // }
 
-// [5-Fetching Data]
+// [5-Fetching Data, 6-Understanding HTTP Requests, 7-Handling Errors]
+// import axios from "axios";
 
-import axios from "axios";
+// interface User {
+//   id: number;
+//   name: string;
+// }
+// function App() {
+//   const [users, setUsers] = useState<User[]>([]);
+//   const [error, setError] = useState("");
+
+//   useEffect(() => {
+// get -> promise -> res / err
+
+//     // This will return Promise
+//     // : An object that holds the eventual result or failure of an asynchronous operation
+//     axios
+//       .get<User[]>("https://jsonplaceholder.typicode.com/xusers")
+//       .then((res) => setUsers(res.data))
+//       .catch((err) => setError(err.message)); // This line will be executed if something goes wrong while fetching the data
+//   }, []);
+//   return (
+//     <>
+//       {error && <p className="text-danger">{error}</p>}
+
+//       <ul>
+//         {users.map((user) => (
+//           <li key={user.id}>{user.name}</li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// }
+
+// [8-Working with Async and Await]
+// Some people don't like .then method of useEffect.
+// Let's rewrite this code without then and catch method.
+import axios, { AxiosError } from "axios";
 
 interface User {
   id: number;
@@ -75,16 +110,36 @@ interface User {
 }
 function App() {
   const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
 
   useEffect(() => {
-    // This will return Promise
-    // : An object that holds the eventual result or failure of an asynchronous operation
-    axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => setUsers(res.data));
+    // async () to make this async. But still doesn' work because react doesn't allow us to pass an async function to the effect hook.
+    const fetchUsers = async () => {
+      try {
+
+        const res = await axios // await function is only allowed  within async functions and at the top of the module
+        .get<User[]>("https://jsonplaceholder.typicode.com/xusers");
+      // get -> await promise -> res / err
+      setUsers(res.data);
+      }
+      catch (err) {
+        setError((err as AxiosError).message);
+      }
+
+    };
+    fetchUsers();
   }, []);
-  return <ul>
-    {users.map(user => <li key={user.id}>{user.name}</li>)}
-  </ul>;
+  return (
+    <>
+      {error && <p className="text-danger">{error}</p>}
+
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
+// Not that elegant. We have to make more functions, more nested {}, format introduced.
 export default App;
