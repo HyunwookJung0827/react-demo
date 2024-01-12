2f0f9d4e11fd87334e2e78cd68bc49b668e120b3
HyunwookJung0827 <as9804@berkeley.edu>
2024-01-12 09:45:30 -0800

    5-Creating a Responsive Layout

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,12 +1,21 @@
 import { useState } from "react";
 import "./App.css";
-import { DatePicker } from "antd";
+import { Col, DatePicker, Row } from "antd";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <Row className="bg-red-400">Nav</Row>
+      <Row>
+        <Col span={12} className="bg-amber-300 aside">
+          Aside
+        </Col>
+        <Col flex="auto" className="bg-blue-500">
+          Main
+        </Col>
+      </Row>
       <h1 className="text-3xl font-bold underline bg-gray-500">Hello world!</h1>
       <DatePicker />
     </>
