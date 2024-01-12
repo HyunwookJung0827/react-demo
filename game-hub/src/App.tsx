802a66dca028c1fe9cb164aa7359ec8f60ab3e50
HyunwookJung0827 <as9804@berkeley.edu>
2024-01-12 13:28:27 -0800

    Implementing the Dark Mode

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,15 +1,20 @@
 import { useState } from "react";
 import "./App.css";
-import { Col, DatePicker, Row } from "antd";
+import { Button, Card, Col, ConfigProvider, DatePicker, Row, theme } from "antd";
 import NavBar from "./components/NavBar";
 
 function App() {
   const [count, setCount] = useState(0);
-
+  const { defaultAlgorithm, darkAlgorithm } = theme;
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const handleClick = () => {
+    setIsDarkMode((previousValue) => !previousValue);
+   };
   return (
-    <>
-      
-      <NavBar />
+    <ConfigProvider theme={{
+      algorithm: isDarkMode ? darkAlgorithm : defaultAlgorithm,
+    }}>
+      <NavBar/>
       <Row>
         <Col span={12} className="bg-amber-300 aside">
           Aside
@@ -20,7 +25,12 @@ function App() {
       </Row>
       <h1 className="text-3xl font-bold underline bg-gray-500">Hello world!</h1>
       <DatePicker />
-    </>
+      <Card style={{ width: "max-content" }}>
+      <Button onClick={handleClick}>
+        Change Theme to {isDarkMode ? "Light" : "Dark"}
+      </Button>
+    </Card>
+    </ConfigProvider>
   );
 }
 
