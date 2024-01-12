7f64f7c04685bd88c1b866e6dda485f142f0eed8
HyunwookJung0827 <as9804@berkeley.edu>
2024-01-12 11:58:21 -0800

    minor

--- game-hub/src/components/NavBar.tsx
+++ game-hub/src/components/NavBar.tsx
@@ -1,14 +1,13 @@
-import { Image, Row } from 'antd';
-import logo from '../assets/Logo/logo.webp';
+import { Image, Row } from "antd";
+import logo from "../assets/Logo/logo.webp";
 
 const NavBar = () => {
-  return (<Row align="middle">
-    <Image src={logo}  alt="Logo" width={60} />
-    NavBar
-   
-  </Row>
-    
-  )
-}
+  return (
+    <Row align="middle">
+      <Image src={logo} alt="Logo" width={60} />
+      NavBar
+    </Row>
+  );
+};
 
-export default NavBar
+export default NavBar;
