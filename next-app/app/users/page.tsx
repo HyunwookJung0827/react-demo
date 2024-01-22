0263ea44f4a29ced04246307b0932d60ace45446
HyunwookJung0827 <as9804@berkeley.edu>
2024-01-22 02:38:02 -0800

    next 9-Caching

--- next-app/app/users/page.tsx
+++ next-app/app/users/page.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 
-const UsersPage = () => {
-  return (
-    <div>UsersPage</div>
-  )
+interface User {
+  id: number;
+  name: string;
 }
 
-export default UsersPage
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    // cache: "no-store", // don't store cache. use this for the data that changes frequently
+    // next: { revalidate: 10 }, // fetch the data from the backend "every 10 seconds"
+  });
+
+  const users: User[] = await res.json();
+  return (
+    <>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default UsersPage;
