e4c004b65f8cda3a503de105eedda7543820ddd1
HyunwookJung0827 <as9804@berkeley.edu>
2024-01-22 02:56:10 -0800

    Finished Next.js Fundamentals

--- next-app/app/users/page.tsx
+++ next-app/app/users/page.tsx
@@ -7,7 +7,7 @@ interface User {
 
 const UsersPage = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users", {
-    // cache: "no-store", // don't store cache. use this for the data that changes frequently
+    cache: "no-store", // don't store cache. use this for the data that changes frequently
     // next: { revalidate: 10 }, // fetch the data from the backend "every 10 seconds"
   });
 
@@ -15,6 +15,7 @@ const UsersPage = async () => {
   return (
     <>
       <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
