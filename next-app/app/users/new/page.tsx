25ef3b40ed3fd3d077154495896c83424670e9cd
HyunwookJung0827 <as9804@berkeley.edu>
2024-01-23 13:32:20 -0800

    next.js Router [8-Programmatic Navigation]

--- next-app/app/users/new/page.tsx
+++ next-app/app/users/new/page.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+"use client";
+// We cannot handle a browser event on server component
+import { useRouter } from "next/navigation"; // Don't import from "next/router"
+import React from "react";
 
 const NewUserPage = () => {
+  const router = useRouter();
   return (
-    <div>NewUserPage</div>
-  )
-}
+    <button className="btn btn-primary" onClick={() => router.push("/users")}>
+      CREATE
+    </button>
+  );
+};
 
-export default NewUserPage
+export default NewUserPage;
