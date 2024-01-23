c60a603a295d548f06fb691f43ac2819ff3d00de
HyunwookJung0827 <as9804@berkeley.edu>
2024-01-23 13:41:07 -0800

    next.js Router [9-Showing Loading UIs]

--- next-app/app/users/page.tsx
+++ next-app/app/users/page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import UserTable from "./UserTable";
 import Link from "next/link";
 
@@ -10,8 +10,12 @@ const UsersPage = async ({ searchParams: { sortOrder } }: Props) => {
     <>
       <h1>Users</h1>
       <p>{new Date().toLocaleTimeString()}</p>
-      <Link href="/users/new" className="btn">New User</Link>
-      <UserTable sortOrder={sortOrder}/>
+      <Link href="/users/new" className="btn">
+        New User
+      </Link>
+      <Suspense fallback={<p>Loading...</p>}>
+        <UserTable sortOrder={sortOrder} />
+      </Suspense>
     </>
   );
 };
