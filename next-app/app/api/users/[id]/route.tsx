646f5350f72717af7ef5d18ef53e617ccd750d64
HyunwookJung0827 <as9804@berkeley.edu>
2024-01-24 06:52:12 -0800

    API [5-Updating an Object]

--- next-app/app/api/users/[id]/route.tsx
+++ next-app/app/api/users/[id]/route.tsx
@@ -18,3 +18,21 @@ export function GET(
 
   return NextResponse.json({ id: 1, name: "Mosh" });
 }
+
+export async function PUT(
+  request: NextResponse,
+  { params }: { params: { id: number } }
+) {
+  // 1. Validate the request body
+  const body = await request.json();
+  // 2. If invalid, return 400
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  // 3. Fetch the user with the given id
+  // 4. If doesn't exist, return 404 not found
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  // 5. Update the user
+  // 6. Return the updated user
+  return NextResponse.json({ id: 1, name: body.name });
+}
