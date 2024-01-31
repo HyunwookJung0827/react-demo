322a2f926bddac2c0f2f76cd52a604afe196142e
HyunwookJung0827 <as9804@berkeley.edu>
2024-01-31 12:23:31 -0800

    Finished E-commerce 16

--- e-shop/app/product/[productId]/ProductDetails.tsx
+++ e-shop/app/product/[productId]/ProductDetails.tsx
@@ -1,7 +1,8 @@
 "use client";
 
+import SetColor from "@/app/components/products/SetColor";
 import { Rating } from "@mui/material";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 interface ProductDetailsProps {
   product: any;
@@ -37,10 +38,20 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
     quantity: 1,
     price: product.price,
   });
-  
+  console.log(cartProduct);
   const productRating =
     product.reviews.reduce((acc: number, item: any) => item.rating + acc, 0) /
     product.reviews.length;
+
+  const handleColorSelect = useCallback(
+    (value: SelectedImgType) => {
+      setCartProduct((prev) => {
+        return { ...prev, selectedImg: value };
+      });
+    },
+    [cartProduct.selectedImg]
+  );
+
   return (
     <div
       className="grid grid-cols-1
@@ -66,7 +77,11 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
           {product.inStock ? "In stock" : "Out of stock"}
         </div>
         <Horizontal />
-        <div>color</div>
+        <SetColor
+          cartProduct={cartProduct}
+          images={product.images}
+          handleColorSelect={handleColorSelect}
+        />
         <Horizontal />
         <div>quantity</div>
         <Horizontal />
